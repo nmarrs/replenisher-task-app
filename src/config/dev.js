a456dafc0e14a3e436d1c6d33406999b089a3294
Nathan Marrs <nathanielmarrs@gmail.com>
2019-03-05 18:17:54 -0700

    Delete dev.js

--- src/config/dev.js
+++
@@ -1,5 +0,0 @@
-export const FirebaseConfig = {
-  apiKey: 'AIzaSyA0O9rRcT3IaZSAZEaq2pbSBwZIdBbx8xs',
-  authDomain: 'replenisher-task-app.firebaseappio.com',
-  databaseURL: 'https://replenisher-task-app.firebaseio.com/'
-}
